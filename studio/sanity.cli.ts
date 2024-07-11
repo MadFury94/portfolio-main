4f39a95fa2729f859b93653e7f2514da28ac403a
MadFury94 <onochieazukaeme@gmail.com>
2024-07-11 13:34:50 +0100

    installed sanity

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'kili4jsv',
-    dataset: 'production',
-  },
+    projectId: '7t4w6jdr',
+    dataset: 'production'
+  }
 })
